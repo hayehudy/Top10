b0c824f15bf5c504b94bf2f4aaaeedc34f5331d9
hayehudy <67315310+hayehudy@users.noreply.github.com>
2021-10-24 21:27:47 +0300

    thirteenth exp with ng

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use('/', express.static('client/React/build'));
-app.use('/', express.static('client/Angular/dist/top10'));
+app.use('/', express.static('client/Angular/dist'));
 
 const MovieSchema = new mongoose.Schema({
   name: String,
